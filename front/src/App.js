5ac6425de4c8c57965e4205446aecffe10eebe26
clokto <copin.thomas@gmail.com>
2018-07-18 15:46:13 +0200

    Installation of react-router-dom and adding route on app.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
 import './App.css'
-import Carou from './components/Carousel.js'
-import MovieCards from './components/MovieCards.js'
-import { Container, Row } from 'reactstrap'
+import Home from './Home'
+import NavBar from './components/NavBar'
+import MovieCards from './components/MovieCards'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 class App extends Component {
   render () {
     return (
-      <Container fluid>
-        {/* <Row>
-          <Carou className='carou' />
-        </Row> */}
-        <MovieCards />
-      </Container>
+      <BrowserRouter>
+        <div>
+          <NavBar />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/movielist' component={MovieCards} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     )
   }
 }
