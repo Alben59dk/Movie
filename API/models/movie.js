60cabf972dd68fbaa7cb0dc288dd67d22f6988b6
Lucas Poirier <poirier.dev@gmail.com>
2018-07-18 10:25:24 +0200

    adding movie model

---
+++ API/models/movie.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+let movieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    require: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  plot: {
+    type: String,
+    required: true
+  },
+  genres: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'GenreModel'
+  },
+  director: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'DirectorModel'
+  },
+  released: {
+    type: Date,
+    required: true
+  },
+  boxOffice: {
+    type: Number,
+    required: false
+  },
+  nationality: {
+    type: String,
+    required: false
+  }
+})
