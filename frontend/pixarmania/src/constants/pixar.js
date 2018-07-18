37a8848b7a74e3457c98ccfb7acc0b4d097a4c8c
Alima Garcia Stéphane <garciastephan60@gmail.com>
2018-07-18 10:55:48 +0200

    adding the route

---
+++ frontend/pixarmania/src/constants/pixar.js
@@ -0,0 +1,22 @@
+const pixar = [{
+avatar: '',
+fullname: 'Coco',
+content: 'Un petit garçon qui aime la musique et rêve de devenir un grand artiste',
+date: '12/12/2016',
+}, {
+avatar: '',
+fullname: 'Les Indestructibles 2',
+content: 'Une famille de super-héros',
+date: '04/07/2018',
+}, {
+avatar: '',
+fullname: 'Coco',
+content: 'histoire un petit garçon qui aime la musique et rêve de devenir un grand artiste',
+date: '12/12/2016',
+}, {
+avatar: '',
+fullname: 'Coco',
+content: 'histoire un petit garçon qui aime la musique et rêve de devenir un grand artiste',
+date: '12/12/2016'
+}
+]
