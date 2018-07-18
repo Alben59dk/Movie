02e3ad19d250e412e188897dd9055d4fb25a5b9c
Alexandre Benard <alben59dk@gmail.com>
2018-07-18 14:17:08 +0200

    import axios

--- movies/src/App.js
+++ movies/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './App.css'
 import SearchBar from './components/SearchBar'
 import VideoList from './components/VideoList'
+import axios from 'axios'
 export default class App extends Component {
   render() {
     return (
