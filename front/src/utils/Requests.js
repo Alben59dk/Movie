810ad3a8cdc92c136b54943c45367f284b7b0633
Louis Bruge <louis.bruge@gmail.com>
2018-07-18 12:25:50 +0200

    Feature (utils/Requests) : create post method

--- front/src/utils/Requests.js
+++ front/src/utils/Requests.js
@@ -8,13 +8,30 @@ export default class Requests {
    *
    * @static
    * @param {string} url url target
-   * @returns {funciton} axios function
+   * @returns {function} axios function
    */
   static get (url) {
     return axios({
       method: 'get',
       baseURL: API_URI,
       url: url
+    })
+  }
+
+  /**
+   * post
+   *
+   * @static
+   * @param {string} url url target
+   * @param {object} data data to send
+   * @returns {function} axios function override
+   */
+  static post (url, data) {
+    return axios({
+      method: 'post',
+      baseURL: API_URI,
+      url: url,
+      data: data
     })
   }
 }
