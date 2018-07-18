f11a0d3ab8473121c9a04ef9492fb90c824cd3b8
nezzar <kefifnezzar@gmai.com>
2018-07-18 18:50:57 +0200

    more filters and sauron button

--- front-office/src/components/List.js
+++ front-office/src/components/List.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap'
+import { Container, Row, Col, Button } from 'reactstrap'
 import { Header, Table, Rating, Image, Form, Input, Divider } from 'semantic-ui-react'
 import axios from 'axios'
 
 import fire from '../images/fire.gif'
+import sauron from '../images/sauron.gif'
 
 function keysUpTo(start, limit){
     let array = []
@@ -31,8 +32,10 @@ class List extends Component {
             filters: {
                 title: '',
                 genre: '',
-                release_date: 1940
-            }
+                release_date: 1940,
+                note: 0
+            },
+            sauron: false
         }
         this.handleChange = this.handleChange.bind(this)
     }
@@ -73,19 +76,28 @@ class List extends Component {
         })
     }
 
+    toSauron(){
+        this.setState({
+            ...this.state,
+            sauron: !this.state.sauron
+        })
+    }
+
     render() {
         if(this.state.movies === null) {return <p>Loading...</p>} else {
         let movies = this.state.movies
                         .filter(movie => movie.title.includes(this.state.filters.title) === true )
                         .filter(movie => (movie.release_date > this.state.filters.release_date) === true)
+                        .filter(movie => (movie.note > this.state.filters.note) === true)
+                        .filter(movie => (this.state.filters.genre === '' ? movie : movie.genre === this.state.filters.genre  ))
         return (           
-        <Container className={'mT-3 mB-3'}>
+        <Container className={this.state.sauron === false ? 'mT-3 mB-3' : 'mT-3 tengan mB-3' }>
         <Row className={''}>
             <Col className={'txtcenter'}>
                 <img className={'txtcenter h100p'} src={fire} size='small' circular />
             </Col>
             <Col className={' vCenter'}>   
-                <h1 className={'w100p txtcenter color-white'}>Listing of the movies</h1>
+                <h1 className={this.state.sauron === false ? ' w100p txtcenter color-white' : ' w100p txtcenter tengan color-white'}>Listing of the movies</h1>
             </Col>
             <Col className={' txtcenter'}>
                 <img className={' txtcenter'} src={fire} size='small' circular />
@@ -93,8 +105,18 @@ class List extends Component {
         </Row>
         <Form>
         <Row className={'brad-10  bg-white'}>
-            <Col>
+            <Col className={'border-green'}>
                 <h1 className={'txtcenter font-b'}><i className={'playstation pink icon'}></i> Mega Filters GFX 300D <i className={'playstation pink icon'}></i></h1>
+                {this.state.sauron === true ?  
+                (<div><h1 className={'txtcenter forder'}>ONE RING TO RULE THEM ALL BITCH!</h1>
+                <div className={'w100p txtcenter'}>
+                    <img className={'txtcenter'} src={sauron}/>
+                </div></div>) : null}
+                <div className={'w100p txtcenter p-2'}>
+                    <Button color='danger' className={'txtcenter'} onClick={this.toSauron.bind(this)}><i className={this.state.sauron === false ? 'txtcenter thermometer icon massive empty': 'txtcenter thermometer icon massive full'}></i></Button>
+
+                </div>
+                    
                 <Form.Group className={'border-coral p-2 brad-10 bg-pink'} widths='equal'>
                     <Form.Field>
                         <label>Title</label>
