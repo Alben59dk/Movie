0aa3bba519e7963a2c95f88268d7d12cc71785cd
Louis Bruge <louis.bruge@gmail.com>
2018-07-18 12:02:38 +0200

    Feature (components) : create CardMovie layouts

---
+++ front/src/components/CardMovie.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap'
+
+export default class CardMovie extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      title: 'la momie',
+      year: 2001,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat'
+    }
+  }
+  render () {
+    return (
+      <Card>
+        <CardImg src='https://placeimg.com/640/480/any' />
+        <CardBody>
+          <CardTitle>{this.state.title}</CardTitle>
+          <CardSubtitle>{this.state.year}</CardSubtitle>
+          <CardText>{this.state.description}</CardText>
+        </CardBody>
+      </Card>
+    )
+  }
+}
