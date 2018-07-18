e522b09ea103763e2ab71906366a37c74ed4a581
Louis Bruge <louis.bruge@gmail.com>
2018-07-18 16:39:35 +0200

    Feature (DetailMovies) : load informations

--- front/src/components/DetailMovie.js
+++ front/src/components/DetailMovie.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 
-import { Row, Col } from 'reactstrap'
+import { Row, Col, Button } from 'reactstrap'
+
+import Requests from '../utils/Requests'
 
 export default class DetailMovie extends Component {
   constructor (props) {
@@ -17,23 +19,49 @@ export default class DetailMovie extends Component {
   }
 
   componentDidMount () {
+    Requests.get(`movie/${this.props.match.params.id}`)
+      .then(response => {
+        this.fillFields(response.data)
+      }
+      )
+      .catch(err => console.log(err))
+  }
 
+  fillFields (movies) {
+    console.log(movies)
+    this.setState({
+      movies: {
+        title: movies.title,
+        year: movies.year,
+        description: movies.description,
+        poster: movies.poster,
+        runtime: movies.runtime
+      }
+    })
   }
 
   render () {
     return (
       <div>
-        <Row>
-          <Col xs={12}>
+        <Row className='justify-content-center'>
+          <Col xs={12} lg={8} >
             <img src='https://placeimg.com/640/480/any' className='img-fluid' alt='Poster' />
           </Col>
         </Row>
         <Row>
-          <Col xs={12}>
-            <h2>{this.state.title}</h2>
-            <p> {this.state.movies.year} </p>
-            <p> {this.state.movies.runtime} </p>
-            <p> {this.state.movies.description} </p>
+          <Col xs={12} lg={9} >
+            <h2 className='text-center'>{this.state.movies.title}</h2>
+            <ul>
+              <li> produit en : {this.state.movies.year} </li>
+              <li> durée : {this.state.movies.runtime} </li>
+              <li> genre : {this.state.movies.genre} </li>
+              <li> {this.state.movies.description} </li>
+            </ul>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button block onClick={this.props.deleteMovies.bind(this, this.props.match.params.id)}>Supprimer le film</Button>
           </Col>
         </Row>
       </div>
