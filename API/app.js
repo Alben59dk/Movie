e208d4443dc2f00a524097669349e87846de199a
Lucas Poirier <poirier.dev@gmail.com>
2018-07-18 11:19:49 +0200

    minor changes

--- API/app.js
+++ API/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const movieRouter = require('./routes/movie')
 
 const app = express()
 
@@ -8,6 +9,9 @@ mongoose.connect(process.env.DB_URI)
 
 app.use(cors())
 app.use(express.json())
+
 app.use('/public', express.static('public'))
+
+app.use('/movie', movieRouter)
 
 module.exports = app
