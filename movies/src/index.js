1ef2367bcadf86d9af25494580d7a4a1bd557838
Alexandre Benard <alben59dk@gmail.com>
2018-07-18 10:20:07 +0200

    install bootstrap & reactstrap

--- movies/src/index.js
+++ movies/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const API_KEY = 'c64a59cbbd683c2d50d4ccb9238eb5bc'
 
