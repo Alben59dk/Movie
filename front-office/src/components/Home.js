e702ce3f9192a5734d26045b455834411b04697f
Maxence Delaine <maxence.delaine@gmail.com>
2018-07-18 17:58:29 +0200

    Updating the home component with a welcome text

---
+++ front-office/src/components/Home.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { Container, Row, Col } from 'reactstrap'
+
+import '../styles/home.css'
+
+class Home extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render () {
+    return (
+      <Container>
+        <Row>
+          <h1 style={{textAlign: 'center', margin: 'auto', padding: '5%'}}> Bienvenue sur le CinéWild! </h1>
+        </Row>
+        <Row>
+          <Col xs={12} lg={8} style={{textAlign: 'center', margin: 'auto', paddingBottom: '5%'}}>
+            <h3> Ce site a été construit avec amour dans le cadre du dernier checkpoint, pour compléter et consulter une liste de films créée de toutes pièces dans une base de données de qualité Bangladesh. </h3>
+            <p>P.S. Les budgets évoqués ne sont pas réels, c&apos;est juste pour la RIGOLADE. AHAH.</p>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
+
+export default Home
