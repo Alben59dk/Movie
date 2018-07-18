be20d141be377df83a31c49f7b8203f32020a0ab
Lucas Poirier <poirier.dev@gmail.com>
2018-07-18 14:01:31 +0200

    added genre router

--- API/app.js
+++ API/app.js
@@ -2,16 +2,31 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const movieRouter = require('./routes/movie')
+const genreRouter = require('./routes/genre')
+const createError = require('http-errors')
+const morgan = require('morgan')
+
+mongoose.connect(process.env.DB_URI)
 
 const app = express()
 
-mongoose.connect(process.env.DB_URI)
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'))
+}
 
 app.use(cors())
 app.use(express.json())
 
 app.use('/public', express.static('public'))
 
+app.use('/genre', genreRouter)
 app.use('/movie', movieRouter)
+
+app.use(function (req, res, next) {
+  console.log('Peut être ici');
+  next(createError(404))
+})
+
+
 
 module.exports = app
