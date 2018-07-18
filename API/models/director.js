f52a59684683522c2f387a7d1e095f0669164e96
Lucas Poirier <poirier.dev@gmail.com>
2018-07-18 10:32:31 +0200

    updating director's infos

---
+++ API/models/director.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+let directorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: false
+  },
+  nationality: {
+    type: String,
+    required: false
+  },
+  birth: {
+    type: Date,
+    required: false
+  }
+})
+
+let DirectorModel = mongoose.model('DirectorModel', directorSchema)
+module.exports = DirectorModel
