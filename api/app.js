e23946dc6f0b9fbb750651036a98d9526440e733
Maxence Delaine <maxence.delaine@gmail.com>
2018-07-18 10:55:58 +0200

    Updating app.js with mongoose and connexion to the database

--- api/app.js
+++ api/app.js
@@ -2,7 +2,8 @@ const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
-const logger = require('morgan')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 const indexRouter = require('./routes/index')
 const moviesRouter = require('./routes/movies')
@@ -13,7 +14,7 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
 
-app.use(logger('dev'))
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
@@ -21,6 +22,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/movies', moviesRouter)
+
+// Connexion to the database
+let uri = 'mongodb://localhost:27017/allocine'
+mongoose.connect(uri, error => {
+  if (error) throw error
+  console.log('Connexion to the DB')
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
