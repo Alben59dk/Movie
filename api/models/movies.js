be46556fb4bd7a2b80e7cd1caea7cf9e35aecd1d
clokto <copin.thomas@gmail.com>
2018-07-18 10:39:18 +0200

    Modification of Movies model

--- api/models/movies.js
+++ api/models/movies.js
@@ -1,72 +1,86 @@
 var mongoose = require('mongoose')
 
 const contactsSchema = new mongoose.Schema({
-  title: {
+  Title: {
     type: String,
     required: true,
     trim: true
   },
-  genre: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  duration: {
+  Year: {
     type: Number,
-    required: true,
-    trim: true
-  },
-  rated: {
-    type: Number,
-    required: true,
-    trim: true
-  },
-  released: {
-    type: Date,
     required: true
   },
-  director: {
+  Rated: {
     type: String,
     required: true,
     trim: true
   },
-  writer: {
+  Released: {
     type: String,
     required: true,
     trim: true
   },
-  plot: {
+  Runtime: {
     type: String,
     required: true,
     trim: true
   },
-  actors: {
+  Genre: {
     type: String,
     required: true,
     trim: true
   },
-  awards: {
+  Director: {
     type: String,
     required: true,
     trim: true
   },
-  poster: {
+  Writer: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  Actors: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  Language: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  Country: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  Awards: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  Poster: {
     type: String,
     required: true
   },
-  metascore: {
+  Metascore: {
     type: Number,
-    required: true,
-    trim: true
-  },
-  boxOffice: {
-    type: Number,
-    required: true,
-    trim: true
-  },
-  production: {
-    type: String,
     required: true
+  },
+  DVD: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  BoxOffice: {
+    type: Number,
+    required: true
+  },
+  Production: {
+    type: String,
+    required: true,
+    trim: true
   }
 })
 
