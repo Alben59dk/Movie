8dfeae7d86613548ed6e19022c8b5b4f59fe095a
clokto <copin.thomas@gmail.com>
2018-07-18 20:37:09 +0200

    Adding some css and className to style the form

--- front/src/components/MovieAdd.js
+++ front/src/components/MovieAdd.js
@@ -38,26 +38,23 @@ export default class MovieAdd extends Component {
     event.preventDefault()
     let dataMovie = this.state.movies
     axios.post(`http://localhost:8080/movies/new`, dataMovie)
-      .then(res => {
-        console.log('OKAY BITCH')
-      })
   }
 
   render () {
     return (
       <Container fluid>
         <Form onSubmit={this.handleSubmit}>
-          <FormGroup row>
+          <FormGroup row className='centered'>
             <Col sm={5}>
-              <Label>Movie title <span className='asterisk'>*</span></Label>
+              <Label>Movie title <span className='addInfoForm'>*</span></Label>
               <Input type='text' name='title' onChange={this.handleChange} />
             </Col>
             <Col sm={5}>
-              <Label>Release year</Label>
+              <Label>Release year <span className='addInfoForm'>Year in XXXX format</span></Label>
               <Input type='text' name='year' onChange={this.handleChange} />
             </Col>
           </FormGroup>
-          <FormGroup row>
+          <FormGroup row className='centered'>
             <Col sm={5}>
               <Label>Runtime</Label>
               <Input type='text' name='runtime' onChange={this.handleChange} />
@@ -67,7 +64,7 @@ export default class MovieAdd extends Component {
               <Input type='text' name='genre' onChange={this.handleChange} />
             </Col>
           </FormGroup>
-          <FormGroup row>
+          <FormGroup row className='centered'>
             <Col sm={5}>
               <Label>Director</Label>
               <Input type='text' name='director' onChange={this.handleChange} />
@@ -77,7 +74,7 @@ export default class MovieAdd extends Component {
               <Input type='text' name='writer' onChange={this.handleChange} />
             </Col>
           </FormGroup>
-          <FormGroup row>
+          <FormGroup row className='centered'>
             <Col sm={5}>
               <Label>Actors</Label>
               <Input type='text' name='actors' onChange={this.handleChange} />
@@ -87,7 +84,7 @@ export default class MovieAdd extends Component {
               <Input type='text' name='plot' onChange={this.handleChange} />
             </Col>
           </FormGroup>
-          <FormGroup row>
+          <FormGroup row className='centered'>
             <Col sm={5}>
               <Label>Language</Label>
               <Input type='text' name='language' onChange={this.handleChange} />
@@ -97,19 +94,19 @@ export default class MovieAdd extends Component {
               <Input type='text' name='awards' onChange={this.handleChange} />
             </Col>
           </FormGroup>
-          <FormGroup row>
+          <FormGroup row className='centered'>
             <Col sm={5}>
-              <Label>Poster <span className='asterisk'>*</span></Label>
+              <Label>Poster <span className='addInfoForm'>* Url of the poster type image</span></Label>
               <Input type='text' name='poster' onChange={this.handleChange} />
             </Col>
             <Col sm={5}>
-              <Label>Metascore</Label>
+              <Label>Metascore <span className='addInfoForm'>Score evaluate on scale going from 0 to 100</span></Label>
               <Input type='text' name='metascore' onChange={this.handleChange} />
             </Col>
           </FormGroup>
-          <FormGroup row>
+          <FormGroup row className='centered'>
             <Col sm={4}>
-              <Label>Box Office</Label>
+              <Label>Box Office <span className='addInfoForm'>Amount with currency</span></Label>
               <Input type='text' name='boxOffice' onChange={this.handleChange} />
             </Col>
             <Col sm={4}>
@@ -121,8 +118,9 @@ export default class MovieAdd extends Component {
               <Input type='text' name='rated' onChange={this.handleChange} />
             </Col>
           </FormGroup>
+          <p className='centered'>* : Obligatory fields </p>
           <FormGroup row>
-            <Col>
+            <Col className='centered'>
               <Button>Adding a movie</Button>
             </Col>
           </FormGroup>
