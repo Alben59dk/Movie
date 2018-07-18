ebb070f0a0b08e43daaacd289690ae2acafbed4a
Alexandre Benard <alben59dk@gmail.com>
2018-07-18 15:48:05 +0200

    delete APi

--- movies/src/index.js
+++ movies/src/index.js
@@ -5,7 +5,5 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const API_KEY = 'c64a59cbbd683c2d50d4ccb9238eb5bc'
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
