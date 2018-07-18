99384d7b8660bbfc16130ec4ca4f49d73c296ced
Lucas Poirier <poirier.dev@gmail.com>
2018-07-18 09:54:21 +0200

    updating app.js

--- API/app.js
+++ API/app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 
 const app = express()
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.DB_URI)
 
 app.use(cors())
 app.use(express.json())
